f974af9fe563c320d4f7a28a769dc13038f79c1b
andrey <kutuzkin98@mail.ru>
2023-08-29 19:19:12 +0800

    Variable adjustment: type - tags

--- parser_app/views.py
+++ parser_app/views.py
@@ -8,12 +8,12 @@ from parser_app.tasks import create_task
 @api_view(['GET', 'POST'])
 def task(request):
     if request.method == 'POST':
-        if "type" in request.data:
-            themes = request.data["type"]
+        if "tags" in request.data:
+            themes = request.data["tags"]
             task = create_task.delay(themes)  # create celery task
             return Response({"message": "Create task", "task_id": task.id, "data": request.data})
         else:
-            return Response({"message": "Error, not found 'type' in POST request"})
+            return Response({"message": "Error, not found 'tags' in POST request"})
     if request.method == 'GET':  # get task status
         if "task_id" in request.data:
             task_id = request.data["task_id"]
