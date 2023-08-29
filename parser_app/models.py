e29ca0cd65a7ea5f021b0a47fbd72dae21a245a7
andrey <kutuzkin98@mail.ru>
2023-08-29 18:28:10 +0800

    Create two model for parsing

--- parser_app/models.py
+++ parser_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseTask(models.Model):
+    """ Celery task info"""
+    themes = models.CharField(max_length=250)
+    count_solution = models.IntegerField(max_length=10)
+    name_and_number = models.CharField(max_length=250)
+    complexity_task = models.IntegerField(max_length=10)
+    is_success = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name_and_number
+
+
+class BaseParsingResult(models.Model):
+    """ Parsing result details"""
+    task_id = models.ForeignKey(
+        BaseTask,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT
+    )
+    data = models.TextField(blank=True)
+    task_type = models.CharField(blank=True, max_length=64)
